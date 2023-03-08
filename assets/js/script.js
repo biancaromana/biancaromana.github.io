01b7d99d010b89146597eac42d9beced07069f0b
Bianca <bk114@hdm-stuttgart.de>
2023-03-08 22:38:20 +0100

    GameMaster Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function moreInfos() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-con");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
